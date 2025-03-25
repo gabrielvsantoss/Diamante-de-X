058e5e01d575274d5ae8484d49cb5186fb209dbf
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-25 16:45:00 -0300

    Criação de um laço de reptição principal e declaração da variavel que vai armazenar o numero digitado

--- Diamante de X/Program.cs
+++ Diamante de X/Program.cs
@@ -1,11 +1,30 @@
-﻿
+﻿using System.ComponentModel;
+
 namespace Diamante_de_X
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Escreva um numero");
+            double numero = Convert.ToInt32(Console.ReadLine());
+            double metade = (numero / 2) + 0.5;
+
+            int quantidade = 1;
+            for(int i = 1; i <= numero; i++)
+            {
+                quantidade++;
+                += quantidade;
+                Console.Write("X");
+                if(quantidade == 2)
+                {
+                    Console.WriteLine("");
+                    quantidade = 0;
+                }
+            }
+
+            Console.WriteLine(metade);
+            Console.ReadLine();
         }
     }
 }
