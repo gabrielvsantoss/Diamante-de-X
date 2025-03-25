a080827eb5b940109b772c72f97577bbff0e80df
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-25 16:00:05 -0300

    Add project files.

---
+++ Diamante de X/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Diamante_de_X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
